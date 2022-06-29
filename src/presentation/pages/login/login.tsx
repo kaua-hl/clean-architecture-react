c235e01aaebb96b76849536bda98a5f3a890fe12
Kauã <profissional.kaua@gmail.com>
2022-06-29 14:27:03 -0300

    feat: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -22,7 +22,7 @@ const Login = () => {
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu E-mail" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
-          <button className={styles.submit} type="submit">Entrar</button>
+          <button disabled data-testid="submit" className={styles.submit} type="submit">Entrar</button>
           <span className={styles.link}>Criar conta</span>
           <FormStatus />
         </form>
