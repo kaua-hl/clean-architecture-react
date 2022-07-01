537a0858f3b06dfa255e7bd84e1436d16d956865
Kauã <profissional.kaua@gmail.com>
2022-07-01 13:41:16 -0300

    feat: ensure ValidationBuilder has an email helper

--- src/presentation/validation/builder/validation-builder.ts
+++ src/presentation/validation/builder/validation-builder.ts
@@ -1,4 +1,5 @@
 import { FieldValidation } from "../protocols/field-validation";
+import { EmailValidation } from "../validators";
 import { RequiredFieldValidation } from "../validators/required-field/required-field-validation";
 
 export class ValidationBuilder {
@@ -13,6 +14,11 @@ export class ValidationBuilder {
 
   required(): ValidationBuilder {
     this.validations.push(new RequiredFieldValidation(this.fieldName));
+    return this;
+  }
+
+  email(): ValidationBuilder {
+    this.validations.push(new EmailValidation(this.fieldName));
     return this;
   }
 
