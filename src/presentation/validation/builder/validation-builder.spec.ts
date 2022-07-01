2bbce98886d2ca90b6f781a0cb128832770ac458
Kauã <profissional.kaua@gmail.com>
2022-07-01 13:41:25 -0300

    test: ensure ValidationBuilder has an email helper

--- src/presentation/validation/builder/validation-builder.spec.ts
+++ src/presentation/validation/builder/validation-builder.spec.ts
@@ -1,3 +1,4 @@
+import { EmailValidation } from "../validators";
 import { RequiredFieldValidation } from "../validators/required-field/required-field-validation";
 import { ValidationBuilder } from "./validation-builder";
 
@@ -5,5 +6,10 @@ describe('ValidationBuilder', () => {
   it('Should return RequiredFieldValidation', () => {
     const validations = ValidationBuilder.field("any_field").required().build();
     expect(validations).toEqual([new RequiredFieldValidation("any_field")]);
+  });
+
+  it('Should return EmailValidation', () => {
+    const validations = ValidationBuilder.field("any_field").email().build();
+    expect(validations).toEqual([new EmailValidation("any_field")]);
   });
 });
