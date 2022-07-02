4d7896dfc9996edddeb164b444443db4124297e7
Kauã <profissional.kaua@gmail.com>
2022-07-02 14:59:04 -0300

    feat: change Login composition to add SaveAccessToken

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,5 +1,6 @@
 import { Login } from "@/presentation/pages"
 import { makeRemoteAuthentication } from "../../usecases/authentication/remote-authentication-factory";
+import { makeLocalSaveAccessToken } from "../../usecases/authentication/save-access-token/local-save-access-token-factory";
 import { makeLoginValidation } from "./login-validation-factory";
 
 export const makeLogin = () => {
@@ -7,6 +8,7 @@ export const makeLogin = () => {
     <Login 
       authentication={makeRemoteAuthentication()} 
       validation={makeLoginValidation()}
+      saveAccessToken={makeLocalSaveAccessToken()}
     />
   );
 }
